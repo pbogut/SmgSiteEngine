1f635195c355248239a2aa21348da19559d3f8ca
Smeagol07 <accounts@smeagol.pl>
2012-06-10 23:27:37 +0200

    Column name for targetUrl changed.

--- src/Smg/MenuBundle/Entity/Item.php
+++ src/Smg/MenuBundle/Entity/Item.php
@@ -36,7 +36,7 @@ class Item
     protected $title;
     
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(name="target_url", type="string", length=255)
      * @var type 
      */
     protected $targetUrl;
